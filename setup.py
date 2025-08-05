e4e8e8dcf53540882f92820017a8bb545e258a21
netranshi Tripathi <netranshitripathi@gmail.com>
2025-08-06 00:00:26 +0530

    files setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    Reads a requirements file and returns a list of packages.
+    
+    """
+    requirements = []
+
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.replace('\n',"") for req in requirements]
+    return requirements
+
+setup(
+    name="ml_project",
+    version="0.0.1",
+    description="A machine learning project",
+    author="netranshi ",
+    author_email="netranshitripathi@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirement.txt")
+)
