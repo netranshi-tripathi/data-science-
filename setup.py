cf4220064da35361980e71edd525a3252f76daa8
netranshi Tripathi <netranshitripathi@gmail.com>
2025-08-06 00:03:17 +0530

    file

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 from typing import List
-
+HYPHEN_E_DOT = "-e ."
 def get_requirements(file_path: str) -> List[str]:
     """
     Reads a requirements file and returns a list of packages.
@@ -11,6 +11,8 @@ def get_requirements(file_path: str) -> List[str]:
     with open(file_path) as file:
         requirements = file.readlines()
         requirements = [req.replace('\n',"") for req in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
     return requirements
 
 setup(
