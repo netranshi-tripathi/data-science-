8326fee2c80951930877fe400ed0ba91886d7234
netranshi Tripathi <netranshitripathi@gmail.com>
2025-08-24 00:14:47 +0530

    model train

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
 import sys
-from src.ml_project.components import data_ingestion
 from src.ml_project.logger import logging
 from src.ml_project.exception import CustomException
 from src.ml_project.components.data_ingestion import DataIngestion
-from src.ml_project.components.data_transformation import DataTransformationConfig, DataTransformation
-from src.ml_project.components.data_ingestion import DataIngestionConfig
-
+from src.ml_project.components.data_transformation import DataTransformation
+from src.ml_project.components.model_trainer import ModelTrainer
 
 if __name__ == "__main__":
     logging.info("Starting the application...")
     try:
-       # data_ingestion_config = DataIngestionConfig()
-        data_ingestion= DataIngestion()
+        # Data Ingestion
+        data_ingestion = DataIngestion()
         train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
 
-       # data_transformation_config = DataTransfortionConfig()
+        # Data Transformation
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(
+        train_array, test_array, _ = data_transformation.initiate_data_transformation(
             train_path=train_data_path,
             test_path=test_data_path
-            )
+        )
+
+        # Model Training
+        model_trainer = ModelTrainer()
+        model_trainer.initiate_model_trainer(
+            train_array=train_array,
+            test_array=test_array
+        )
 
     except Exception as e:
-        logging.info("custom exception")
-        raise CustomException(e, sys) 
-
-       
-    except Exception as e:
-        logging.info("custom exception")
-        raise CustomException(e, sys) 
+        logging.info("Custom exception occurred")
+        raise CustomException(e, sys)
--- src/ml_project/utils.py
+++ src/ml_project/utils.py
@@ -2,6 +2,8 @@ import os
 import sys
 from src.ml_project.exception import CustomException
 from src.ml_project.logger import logging
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 import pandas as pd
 from dotenv import load_dotenv
 import pymysql
@@ -44,3 +46,24 @@ def save_object(file_path, obj):
             pickle.dump(obj, f)
     except Exception as e:
         raise CustomException(e, sys) from e
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+            # Here we can add hyperparameter tuning if needed
+            gs= GridSearchCV(model,param,cv=3)
+            gs.fit(X_train,y_train)
+           
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+        return report
+    except Exception as e:
+        raise CustomException(e, sys)
